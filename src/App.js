aba5583cb9fe94794d3f1c1525a29557b466c03c
Parmanand Sahadeo <parmsahadeo530@gmail.com>
2021-04-27 13:26:47 -0400

    Adds redirect routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -60,15 +60,32 @@ import { setCurrentUser } from './redux/user/user.actions';
 				<Switch>
 					<Route exact path='/' component={HomePage} />
 					<Route path='/shop' component={ShopPage} />
-					<Route path='/sign-in' component={SignInAndSignUp} />
-				</Switch>
+					<Route 
+						exact 
+						path='/sign-in' 
+						render={() =>
+							this.props.currentUser ? (
+								<Redirect to='/' />
+							) : (
+								<SignInAndSignUp />
+							)
+						}
+					/>
+				</Switch> 
 			</div>
 		);
 	}
 }
 
+const mapStateToProps = ({ user }) => ({
+	currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
 	setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+	mapStateToProps, 
+	mapDispatchToProps
+)(App);
